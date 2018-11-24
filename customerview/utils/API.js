41562b14da605ccc931190e28a3866e58adc20cc
Sutras <37882933+Audiosutras@users.noreply.github.com>
2018-11-24 13:24:32 -0500

    Added Dish routes (Menu) to API.js in customerview

--- customerview/utils/API.js
+++ customerview/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
     //Restaurant Routes
-    getAllRestaurants: query => (query) ? axios.get(`/api/restaurants/${query}`) : axios(`/api/restaurants`),
+    getAllRestaurants: query => (query) ? axios.get(`/api/restaurants/${query}`) : axios.get(`/api/restaurants`),
     createRestaurant: data => axios.post(`/api/restaurants/`, data),
     getRestaurant: restaurantId => axios.get(`/api/restaurants/${restaurantId}`),
         //update Restaurant's general information
@@ -13,7 +13,15 @@ export default {
     addDishToRestaurant: (restaurantId, data) => axios.put(`/api/restaurants/add-dishes/${restaurantId}`, data),
     removeDishFromRestaurant: (restaurantId, data) => axios.put(`/api/restaurants/remove-dishes/${restaurantId}`, data),
 
-    
+    //Dish Routes
+    getDishesAcrossRestaurants: query => (query) ? axios.get(`/api/dishes/${query}`) : axios.get(`/api/dishes/`),
+    getDish: dishId => axios.get(`/api/dishes/${dishId}`),
+    updateDish: (dishId, data) => axios.put(`/api/dishes/${dishId}`, data),
+    removeDish: dishId => axios.delete(`/api/dishes/${dishId}`),
+    createDish: (restaurantId, data) => axios.post(`/api/dishes/restaurant/${restaurantId}`, data),
+
+    //Table Routes
+
 
 
 }
