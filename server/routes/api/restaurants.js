af04dac5e8da668a09398464b759980191b0e9a5
Sutras <37882933+Audiosutras@users.noreply.github.com>
2018-11-26 21:08:49 -0500

    need to update server-side routes as well for creating a restaurant

--- server/routes/api/restaurants.js
+++ server/routes/api/restaurants.js
@@ -4,12 +4,12 @@ const restaurantController = require("../../controllers/restaurantController");
 // "/api/restaurants/"
 router
     .route("/")
-    .get(restaurantController.findAll)
-    .post(restaurantController.create);
+    .get(restaurantController.findAll);
 // "/api/restaurants/:id"
 router
     .route("/:id")
     .get(restaurantController.findById)
+    .post(restaurantController.create)
     .put(restaurantController.update)
     .delete(restaurantController.removeRestaurant);
 // "/api/restaurants/add-orders/:id"
